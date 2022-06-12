b0b36dc5a1a4e95185f64f729e8e67a1a68c0c24
#0001-2022-05-16-09:30:07AM <104608815+FW4248@users.noreply.github.com>
2022-06-12 18:20:12 +0800

    Initialize dependabot/npm_and_yarn/karma-6.3.16

---
+++ src/server/src/documents/cla.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const CLASchema = mongoose.Schema({
+    created_at: Date,
+    revoked_at: Date,
+    end_at: Date,
+    custom_fields: String,
+    gist_url: String,
+    gist_version: String,
+    owner: String,
+    ownerId: String,
+    repo: String,
+    repoId: String,
+    org_cla: Boolean,
+    user: String,
+    userId: String,
+    origin: String,
+    updated_at: Date
+})
+
+
+const CLA = mongoose.model('CLA', CLASchema)
+
+/* CLA.collection.dropAllIndexes(function (err, results) {
+    if (err) {
+        logger.warn('CLA collection dropAllIndexes error: ', err)
+        logger.warn('dropAllIndexes results: ', results)
+    }
+}) */
+
+// add a full wildcard index for better indexing
+CLA.collection.createIndex({ '$**' : 1 })
+module.exports = {
+    CLA: CLA
+}
